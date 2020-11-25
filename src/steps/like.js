294b408dfec31826bfdf604eb9d3177caa0ec2ff
IvanPatskan <64608047+IvanPatskan@users.noreply.github.com>
2020-11-25 10:35:38 +0200

    Update like.js

--- src/steps/like.js
+++ src/steps/like.js
@@ -4,22 +4,22 @@ Given('I am on the login page do login', function () {
 		//browser.url('https://test.lumen.ceek.com/login/');
 		browser.url('https://www.ceek.com//login/');
 		
-		browser.pause(2000)
+		browser.pause(1000)
 });
 
 When('I log in and go to discover and click on like button', function () {
 		browser.$('//*[@id="si-email"]').setValue('mewiwev62879@jqjlb.com')
-		browser.pause(2000)
+		browser.pause(1000)
 		browser.$('//*[@id="si-password"]').setValue('qwerty123')
-		browser.pause(2000)
+		browser.pause(1000)
 		browser.$('//*[@id="login"]').click()
-		browser.pause(2000)
+		browser.pause(1000)
 		browser.url('https://www.ceek.com/discover/');
-		browser.pause(2000)
+		browser.pause(1000)
 		browser.$('/html/body/div/main/div[2]/div[2]/section/article[1]/div/div[2]/div[2]').click()
 		//browser.$('/html/body/div/main/div[2]/div[2]/section/article[2]/div[1]').click()
 
-		browser.pause(2000)
+		browser.pause(1000)
 });
 
 Then('The video must be liked', function () {
@@ -28,6 +28,7 @@ Then('The video must be liked', function () {
 	browser.pause(2000)
 	var datainfo = browser.$('/html/body/div/main/div/div/div[4]/section/article[1]/div[1]/div[2]/div[2]').getAttribute('data-info');
 	browser.$('/html/body/div/main/div/div/div[4]/section/article[1]/div[1]/div[2]/div[2]').click()
+	browser.pause(1000)
 	expect(datainfo).toEqual('366')
 	//expect(n).toEqual('Avatar')
 });
